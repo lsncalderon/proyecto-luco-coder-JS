e0383f7d369116058b0b2dca31fc8e120125f62e
Luisana Calderon <Lsncalderon21@gmail.com>
2023-04-01 16:37:05 -0300

    Entrega-1

---
+++ main.js
@@ -0,0 +1,53 @@
+//Simulador de venta de productos para el hogar, se puede comprar con o sin Gift card de descuento//
+
+//Variable del codigo Gift card
+
+let giftCard = "1234-5678";
+
+alert ("Bienvenidos a tienda Luco, encontraras cosas lindas para tu hogar.")
+
+let acceso= prompt("¿Tienes una GiftCard de regalo?. Ingresá el código.")
+    if(acceso === giftCard){
+        alert ("Tienes un cupón de descuento por 3000 pesos.")
+        giftCard=true;
+    }
+    else{
+        alert ("No tienes un cupón de descuento, no te preocupes puedes continuar tu compra.")
+        giftCard=false;
+    }
+
+//Funcion de comprar
+function compraLuco (){
+    let valorGift = 3000;
+    let valorMaceta = 6500;
+    let valorPortavasos =4500;
+    let opciones = prompt ("Elegí una de las opciones del menú:\n\n1-Macetas: 6500 pesos \n2-Portavasos cuadrados: 4500 pesos \n\nEscribe -Salir- si no deseas comprar")
+
+    while (opciones != "Salir"){
+
+        if (opciones == "1" && giftCard==true){
+            let valorTotal = valorMaceta-valorGift
+            alert (`El precio total de tu compra es: ${valorTotal} pesos`);
+
+        }else if (opciones == "1" && giftCard==false){
+            alert (`El precio total de tu compra es: ${valorMaceta} pesos`);
+
+        }else if (opciones == "2" && giftCard==true){
+            let valorTotal = valorPortavasos-valorGift
+            alert (`El precio total de tu compra es: ${valorTotal} pesos`);
+
+        }else if (opciones == "2" && giftCard==false){
+            alert (`El precio total de tu compra es: ${valorPortavasos} pesos`);
+
+        }else if (opciones === null) { // el usuario pone “Cancelar”
+        break; // salimos del bucle
+
+        }else {
+            alert ("Ninguna de las opciones es correcta");
+        }
+        opciones = prompt ("Elegí una de las opciones del menú: \n1-Macetas: 6500 pesos \n2-Portavasos cuadrados: 4500 pesos \n\nEscribe -Salir- si no deseas comprar")
+    }
+    alert('Gracias por tu visita. ¡Vuelve pronto!');
+}
+
+compraLuco();
