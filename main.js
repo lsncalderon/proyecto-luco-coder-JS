94e0e7e123d8805d74f92f992b9d3092e2e988f9
Luisana Calderon <Lsncalderon21@gmail.com>
2023-04-22 17:04:53 -0300

    ForEach

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 //Simulador de venta de productos para el hogar, se puede comprar con o sin Gift card de descuento. El objetivo del simulador es probar que la gift card de descuento funciona y que da el valor total de la compra con descuento, o permite comprar sin giftcard//
 
-
 class Objetos {
     constructor (nombre, color, material, precio){
         this.nombre = nombre;
@@ -20,9 +19,10 @@ let opciones= true;
 let unidadesCantidades = true;
 
 
-let giftCard = "1234-5678";  /* Codigo para comprar con descuento */
+let giftCard = "1234-5678";  //Codigo para comprar con descuento
 let valorGift = 3000;
-let valorMaceta = 6500;
+let valorMaceta1 = 6500;
+let valorMaceta2 = 6000
 
 alert ("Bienvenidos a tienda Luco, encontraras cosas lindas para tu hogar.")
 
@@ -41,30 +41,30 @@ while (opciones) {
 
         if (opciones == "1" && giftCard==true){
             carrito.push(listaCompra[0].precio)
-            let valorTotal = valorMaceta-valorGift
-            alert('Producto a comprar: \n' + '\n' + listaCompra[0].nombre + '\n' + "Color: " + listaCompra[0].color + '\n'+ "Material: " + listaCompra[0].material + '\n' + "Precio: " + listaCompra[0].precio)
-            alert('Tu compra tiene un descuento: \n' + '\n' + listaCompra[0].nombre  +  "\n" + "Precio total: " + valorTotal)
+            let valorTotal = valorMaceta1-valorGift
+            alert('Producto a comprar: \n' + '\n' + listaCompra[0].nombre + '\n' + "Color: " + listaCompra[0].color + '\n'+ "Material: " + listaCompra[0].material + '\n' + "Precio por unidad: " + listaCompra[0].precio)
+            alert('Tu compra tiene un descuento: \n' + '\n' + listaCompra[0].nombre  +  "\n" + "Precio por unidad: " + valorTotal)
             unidades ();
             break;
           
          }else if (opciones == "1" && giftCard==false){
             carrito.push(listaCompra[0].precio)
-            alert('Producto a comprar: \n' + '\n' + listaCompra[0].nombre + '\n' + "Color: " + listaCompra[0].color + '\n'+ "Material: " + listaCompra[0].material + '\n' + "Precio: " + listaCompra[0].precio)
+            alert('Producto a comprar: \n' + '\n' + listaCompra[0].nombre + '\n' + "Color: " + listaCompra[0].color + '\n'+ "Material: " + listaCompra[0].material + '\n' + "Precio por unidad: " + listaCompra[0].precio)
             unidades ();
             break;
 
         }else if (opciones == "2" && giftCard==true){
             carrito.push(listaCompra[1].precio)
-            let valorTotal = valorMaceta-valorGift
-           listaCompra.push (opciones)
-            alert('Producto a comprar: \n' + '\n' + listaCompra[1].nombre + '\n' + "Color: " + listaCompra[1].color + '\n'+ "Material: " + listaCompra[1].material + '\n' + "Precio: " + listaCompra[1].precio)
-            alert('Tu compra tiene un descuento: \n' + '\n' + listaCompra[1].nombre  +  "\n" + "Precio total: " + valorTotal)
+            let valorTotal = valorMaceta2-valorGift
+            listaCompra.push (opciones)
+            alert('Producto a comprar: \n' + '\n' + listaCompra[1].nombre + '\n' + "Color: " + listaCompra[1].color + '\n'+ "Material: " + listaCompra[1].material + '\n' + "Precio por unidad: " + listaCompra[1].precio)
+            alert('Tu compra tiene un descuento: \n' + '\n' + listaCompra[1].nombre  +  "\n" + "Precio por unidad: " + valorTotal)
             unidades ();
             break;
 
         }else if (opciones == "2" && giftCard==false){
             carrito.push(listaCompra[1].precio)
-            alert('Producto a comprar: \n' + '\n' + listaCompra[1].nombre + '\n' + "Color: " + listaCompra[1].color + '\n'+ "Material: " + listaCompra[1].material + '\n' + "Precio: " + listaCompra[1].precio)
+            alert('Producto a comprar: \n' + '\n' + listaCompra[1].nombre + '\n' + "Color: " + listaCompra[1].color + '\n'+ "Material: " + listaCompra[1].material + '\n' + "Precio por unidad: " + listaCompra[1].precio)
             unidades ();
             break;
 
@@ -80,7 +80,9 @@ while (opciones) {
             cantidades = parseInt(prompt("Ingresá la cantidad de unidades ó -Cancelar- si no deseas seguir"));
             while (unidadesCantidades) {
                 if (cantidades > 0) {
-                  
+                  alert ("Sumamos a tu lista la cantidad de: " + cantidades)
+                  resultado();
+                  break;
                 } else if (cantidades == 0) {
                     unidadesCantidades = false;
                 } else {
@@ -88,15 +90,17 @@ while (opciones) {
                     cantidades = parseInt(prompt("Ingresá la cantidad de unidades -Cancelar- si no deseas seguir"));
                 }
             }
-        }
-    
+        }  
+    }    
+
+    function resultado (){
+        listaCompra.forEach(carrito=>{
+            alert (carrito + " Esta es tu compra final")
+        })
     }
- 
-alert('Gracias por tu visita. ¡Vuelve pronto!');
 
     
-
-
-
-
+    alert('Gracias por tu visita. ¡Vuelve pronto!');
+    
+    
 
